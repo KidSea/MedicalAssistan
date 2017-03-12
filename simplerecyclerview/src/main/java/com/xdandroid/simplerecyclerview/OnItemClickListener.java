82b71b5b63dc1f646aaa37439fce8702e1836c9b
KidSea <594643219@qq.com>
2017-03-12 16:54:04 +0800

    修改gradle配置

--- simplerecyclerview/src/main/java/com/xdandroid/simplerecyclerview/OnItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.xdandroid.simplerecyclerview;
-
-import android.support.v7.widget.*;
-import android.view.*;
-
-public interface OnItemClickListener {
-    void onItemClick(RecyclerView.ViewHolder holder, View v, int position, int viewType);
-}
--- simplerecyclerview/src/main/java/com/xdandroid/simplerecyclerview/OnItemLongClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.xdandroid.simplerecyclerview;
-
-import android.support.v7.widget.*;
-import android.view.*;
-
-public interface OnItemLongClickListener {
-    boolean onItemLongClick(RecyclerView.ViewHolder holder, View v, int position, int viewType);
-}
