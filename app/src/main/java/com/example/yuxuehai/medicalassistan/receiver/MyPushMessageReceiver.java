5e9bf3489e41637d3f9185e332741ad1780e5b8a
KidSea <594643219@qq.com>
2017-05-11 13:29:56 +0800

    完善消息通知功能

--- app/src/main/java/com/example/yuxuehai/medicalassistan/receiver/MyPushMessageReceiver.java
+++ app/src/main/java/com/example/yuxuehai/medicalassistan/receiver/MyPushMessageReceiver.java
@@ -9,14 +9,17 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.NotificationCompat;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.example.yuxuehai.medicalassistan.R;
+import com.example.yuxuehai.medicalassistan.bean.Notifications;
 import com.example.yuxuehai.medicalassistan.ui.MainActivity;
+import com.example.yuxuehai.medicalassistan.utlis.GsonUtils;
+import com.example.yuxuehai.medicalassistan.utlis.ToastUtil;
 import com.example.yuxuehai.medicalassistan.utlis.UIUtils;
 
 import cn.bmob.push.PushConstants;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 
 public class MyPushMessageReceiver extends BroadcastReceiver {
 
@@ -26,37 +29,62 @@ public class MyPushMessageReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO Auto-generated method stub
         if (intent.getAction().equals(PushConstants.ACTION_MESSAGE)) {
-            Log.d("bmob", "BmobPushDemo收到消息：" + intent.getStringExtra(PushConstants.
-                    EXTRA_PUSH_MESSAGE_STRING));
-            Toast.makeText(context, "BmobPushDemo收到消息：" + intent.getStringExtra(PushConstants.
-                    EXTRA_PUSH_MESSAGE_STRING), Toast.LENGTH_SHORT).show();
+//            Log.d("bmob", "BmobPushDemo收到消息：" + intent.getStringExtra(PushConstants.
+//                    EXTRA_PUSH_MESSAGE_STRING));
+//            Toast.makeText(context, "BmobPushDemo收到消息：" + intent.getStringExtra(PushConstants.
+//                    EXTRA_PUSH_MESSAGE_STRING), Toast.LENGTH_SHORT).show();
 
-            NotificationManager nm = (NotificationManager) context.
-                    getSystemService(Context.NOTIFICATION_SERVICE);
+            Notifications notifications = GsonUtils.parseJsonWithGson(intent.
+                    getStringExtra(PushConstants.EXTRA_PUSH_MESSAGE_STRING), Notifications.class);
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(UIUtils.
-                    getContext());
+            saveNotification(context, notifications);
 
-
-            
-            Intent activityIntent =  new Intent(context, MainActivity.class);
-            activityIntent.putExtra("Notification", "notification");
-            activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 , activityIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-
-            builder.setContentTitle("收到通知啦")
-                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_logo_no))
-                    .setSmallIcon(R.drawable.ic_logo_no)
-                    .setWhen(System.currentTimeMillis())
-                    .setContentIntent(pendingIntent)
-                    .setContentText(intent.getStringExtra(PushConstants.
-                            EXTRA_PUSH_MESSAGE_STRING))
-                    .setColor(Color.parseColor("#3bc06b"))
-                    .setAutoCancel(true)
-                    .setPriority(Notification.PRIORITY_DEFAULT);
-            nm.notify(1, builder.build());
+            createNotification(context, notifications);
         }
+    }
+
+    private void createNotification(Context context, Notifications notifications) {
+        NotificationManager nm = (NotificationManager) context.
+                getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(UIUtils.
+                getContext());
+
+        Intent activityIntent =  new Intent(context, MainActivity.class);
+        activityIntent.putExtra("Notification", "notification");
+        activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 , activityIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentTitle("收到通知啦")
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_logo_no))
+                .setSmallIcon(R.drawable.ic_logo_no)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setContentText(notifications.getContent())
+                .setColor(Color.parseColor("#3bc06b"))
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_DEFAULT);
+        nm.notify(1, builder.build());
+    }
+
+    private void saveNotification(Context context, Notifications notifications) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                notifications.save(new SaveListener<String>() {
+                    @Override
+                    public void done(String s, BmobException e) {
+                        if (e == null){
+                            ToastUtil.showToast(context, "保存成功");
+                        }else {
+                            ToastUtil.showToast(context, "保存失败");
+                        }
+                    }
+                });
+            }
+        }).start();
     }
 
 }
